f025f5ac968885125248c3d723e608ac4c704b2d
hiroshi8818 <hshu8818@gmail.com>
2026-01-05 22:39:16 -0500

    add persistence for ingredient

---
+++ src/main/java/tacocloud/repository/IngredientRepository.java
@@ -0,0 +1,10 @@
+package tacocloud.repository;
+
+
+import tacocloud.model.Ingredient;
+
+public interface IngredientRepository {
+    Iterable<Ingredient> findAll();
+    Ingredient findOne(String id);
+    Ingredient save(Ingredient ingredient);
+}
